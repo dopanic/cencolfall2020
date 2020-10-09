63ba24399326222214a71816710aa2a2cd0317af
Jonny Park <kw1@live.com>
2020-10-09 17:38:18 -0400

    changed: the autoBanner logic

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,36 +4,39 @@
 //  Date: 7/10/2020
 //  Stitched by Jonny with LOVE
 
-//// index.ejs
-// auto scroll banner: banner scrolling using hidden overflow
-// random integer generator
+//// General
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-
-// set the variables
-if (document.querySelector('.autoScrollBanner')) {
-    // count the words prepared for the hi-box
-    var banner_list = document.querySelectorAll('.autoScrollBanner__list'); // get the banner list
-    var banner_counts = banner_list.length;
-    var banner_height = banner_list[0].clientHeight;
-}
-var random_int = 0;
-var target_list = 0;
-
-function autoScrollBanner() {
-    var random_int = getRndInteger(0, (banner_counts - 1)); // get the random number by the number of banners
-     // if the random number is same as the last, get it again
-    if (random_int === target_list) random_int = getRndInteger(0, (banner_counts - 1));
-    target_list = random_int;
-    // banner scoll animation
-    $('.autoScrollBanner__list').animate({
-        top: '-' + banner_height * target_list + 'px'
+function smoothScrollTo(target, order) {
+    var target_height = document.querySelector(target).clientHeight;
+    var new_scroll_position = order * target_height;
+    $(target).animate({
+        scrollTop: new_scroll_position + 'px'
     });
+    return new_scroll_position;
 }
 
-window.addEventListener('load', (event) => { autoScrollBanner(); }); // when loaded, scroll the banner
-setInterval(() => { autoScrollBanner(); }, 2000);
+
+//// index.ejs
+// automatic scrolling banner: banner scrolling using hidden overflow
+function autoRndBannerScrolling(target_class) {
+    do {
+        var number_of_targets = document.querySelector(target_class).childElementCount; // get the numbet of targets
+        var random_int = getRndInteger(0, (number_of_targets - 1)); // get the random number within the number of targets
+        var current_scroll_position = smoothScrollTo(target_class, random_int);
+    } while (current_scroll_position === $(target_class).scrollTop()); // if the new position is same as the last, loop
+}
+
+
+//// project.ejs
+// var test = document.querySelectorAll(".article");
+// autoScrollBanner(test);
+
+
+
+window.addEventListener('load', (event) => { autoRndBannerScrolling('.autoScrollBanner'); }); // when loaded, scroll the banner
+setInterval(() => { autoRndBannerScrolling('.autoScrollBanner'); }, 2500);
 
 
 //// contactme.ejs
